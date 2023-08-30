29909508361ec96248cce003a3b0affc752825a1
Namit Piriya <34130842+namit-piriya@users.noreply.github.com>
2023-08-31 02:25:06 +0530

    Add contains duplicate

--- ArrayProblems.java
+++ ArrayProblems.java
@@ -30,22 +30,22 @@ public class ArrayProblems {
         return sol;
     }
 
-    public static int[] twoSumHash(int[] nums, int target){
-        HashMap<Integer,Integer> indexes = new HashMap<>();
+    public static int[] twoSumHash(int[] nums, int target) {
+        HashMap<Integer, Integer> indexes = new HashMap<>();
         int i = 0;
-        int [] ans = new int[2];
-        for(int num : nums){
-            indexes.put(num,i++);
+        int[] ans = new int[2];
+        for (int num : nums) {
+            indexes.put(num, i++);
         }
         i = 0;
-        for(int num : nums){
+        for (int num : nums) {
             int currTarget = target - num;
 
-             if(indexes.containsKey(currTarget) && indexes.get(currTarget) != i){
-                 ans[0] = indexes.get(currTarget);
-                 ans[1] = i;
-             }
-             i++;
+            if (indexes.containsKey(currTarget) && indexes.get(currTarget) != i) {
+                ans[0] = indexes.get(currTarget);
+                ans[1] = i;
+            }
+            i++;
         }
         return ans;
     }
@@ -54,13 +54,34 @@ public class ArrayProblems {
     public static int maxProfit(int[] prices) {
         int min = Integer.MAX_VALUE;
         int ans = 0;
-        for(int price : prices){
-            if(min > price ){
+        for (int price : prices) {
+            if (min > price) {
                 min = price;
             }
-            ans = Math.max(ans,price-min);
+            ans = Math.max(ans, price - min);
         }
         return ans;
     }
+
+
+    // https://leetcode.com/problems/contains-duplicate/
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> e = new HashSet<Integer>();
+
+        for (int num : nums) {
+            if (e.contains(num)) {
+                return true;
+            } else
+                e.add(num);
+        }
+        return false;
+    }
+
+    //https://leetcode.com/problems/product-of-array-except-self/
+    public int[] productExceptSelf(int[] nums) {
+
+    }
+
+
 
 }
