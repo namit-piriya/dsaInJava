bce87889463ef51cc9c88a789bac5971c8045a6e
Namit Piriya <34130842+namit-piriya@users.noreply.github.com>
2023-09-10 04:06:31 +0530

    [Tree] distance k, maxLevelSum,

--- TreeProblems.java
+++ TreeProblems.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TreeProblems {
     public void inorder(TreeNode root, List<Integer> traversal) {
@@ -71,8 +70,99 @@ public class TreeProblems {
         } else if (right == null) {
             return left;
         } else return root;
-
     }
+
+
+
+//    https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        if (root == null) return null;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        List<Integer> ans = new ArrayList<>();
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            var qSize = q.size();
+            while (qSize-- > 0) {
+                var node = q.poll();
+                if (node.left != null) {
+                    parent.put(node.left, node);
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    parent.put(node.right, node);
+                    q.offer(node.right);
+                }
+            }
+        }
+
+        Set<TreeNode> s = new HashSet<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            var qSize = q.size();
+            while (qSize-- > 0) {
+                var node = q.poll();
+                if (node.val == target.val) {
+                    this.findLevel(node, s, k, 0, ans, parent);
+                    return ans;
+                }
+                if (node.left != null) {
+                    parent.put(node.left, node);
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    parent.put(node.right, node);
+                    q.offer(node.right);
+                }
+            }
+        }
+        return ans;
+    }
+
+    private void findLevel(TreeNode node, Set<TreeNode> visited,
+                           int maxLevel,
+                           int level, List<Integer> ans, Map<TreeNode, TreeNode> parentMap) {
+        if (level > maxLevel) return;
+        if (node == null) return;
+        if (visited.contains(node)) {
+            return;
+        }
+        if (level == maxLevel) {
+            ans.add(node.val);
+        }
+        visited.add(node);
+        findLevel(node.left, visited, maxLevel, level + 1, ans, parentMap);
+        findLevel(node.right, visited, maxLevel, level + 1, ans, parentMap);
+        findLevel(parentMap.get(node), visited, maxLevel, level + 1, ans, parentMap);
+    }
+
+
+//    https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+    public int maxLevelSum(TreeNode root) {
+        if(root == null) return 0;
+        int ans = Integer.MIN_VALUE;
+        var queue = new ArrayDeque<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            var currSum = 0;
+            var queueSize = queue.size();
+            while(queueSize-- > 0){
+                var node = queue.poll();
+                if(node.left != null)
+                    queue.offer(node.left);
+                if(node.right != null)
+                    queue.offer(node.right);
+                currSum += node.val;
+            }
+            if(currSum > ans) {
+                ans = currSum;
+            }
+        }
+        return ans;
+    }
+
+
+
 
 
 }
