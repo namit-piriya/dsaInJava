f190dc3490030e0baccac55a9c3a12819e415797
Namit Piriya <34130842+namit-piriya@users.noreply.github.com>
2023-09-12 02:24:00 +0530

    [Array] https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to

--- ArrayProblems.java
+++ ArrayProblems.java
@@ -117,7 +117,7 @@ public class ArrayProblems {
     public static int removeDuplicates(int[] nums) {
         int curr = 2;
         for (int i = 2; i < nums.length; i++) {
-            if(nums[i] != nums[curr-2]){ // curr is the array index without duplicates more than 2
+            if (nums[i] != nums[curr - 2]) { // curr is the array index without duplicates more than 2
                 nums[curr] = nums[i];
                 curr++;
             }
@@ -125,8 +125,8 @@ public class ArrayProblems {
         return curr;
     }
 
-    public static void reverse(int[] nums, int s , int e){
-        while(s<e){
+    public static void reverse(int[] nums, int s, int e) {
+        while (s < e) {
             int t = s;
             s = e;
             e = t;
@@ -136,13 +136,13 @@ public class ArrayProblems {
     }
 
     public static void rotate(int[] nums, int k) {
-        k%=(nums.length-1);
-        reverse(nums,0,nums.length-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k, nums.length-1);
+        k %= (nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
     }
 
-//    You can sell the stock every day if the price is higher
+    //    You can sell the stock every day if the price is higher
 //    anyways you can only hold one share
 //    1,3,5
 //    1,3 and 3,5 is same as 1,5
@@ -156,18 +156,18 @@ public class ArrayProblems {
         return maxProfit;
     }
 
-//    pascal triangle
+    //    pascal triangle
     public List<List<Integer>> generate(int numRows) {
-        List <List<Integer>> ans = new ArrayList<>();
-        if(numRows <= 0)return ans;
+        List<List<Integer>> ans = new ArrayList<>();
+        if (numRows <= 0) return ans;
         var list = new Integer[]{1};
         ans.add(Arrays.asList(list));
         for (int i = 1; i < numRows; i++) {
-            var previousRow = ans.get(i-1);
+            var previousRow = ans.get(i - 1);
             var currentRow = new ArrayList<Integer>();
             currentRow.add(1);
-            for (int j = 1; j <previousRow.size(); j++) {
-                int sum = previousRow.get(j) + previousRow.get(j-1);
+            for (int j = 1; j < previousRow.size(); j++) {
+                int sum = previousRow.get(j) + previousRow.get(j - 1);
                 currentRow.add(sum);
             }
             currentRow.add(1);
@@ -182,6 +182,41 @@ public class ArrayProblems {
         int n = removeDuplicates(nums);
         System.out.println("reflected size is " + n);
         System.out.println(Arrays.toString(nums));
+    }
+
+//    https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        Map<Integer, List<Integer>> m = new HashMap<>();
+        for (int i = 0; i < groupSizes.length; i++) {
+            var num = groupSizes[i];
+            var numList = m.getOrDefault(num, new ArrayList<Integer>());
+            numList.add(i);
+            m.put(num, numList);
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for (var entries : m.entrySet()) {
+            var size = entries.getKey();
+            var list = entries.getValue();
+            var listSize = list.size();
+            while (listSize > 0) {
+                var ptr = 0;
+                var subAns = new ArrayList<Integer>();
+                var currSize = size;
+                while (currSize-- > 0) {
+                    var val = list.get(ptr++);
+                    subAns.add(val);
+                }
+                ans.add(subAns);
+                listSize -= size;
+            }
+
+        }
+        return ans;
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+
     }
 
 }
