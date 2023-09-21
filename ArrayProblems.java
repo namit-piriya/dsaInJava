b3a137a9f1735b2eaaeb197a83e64c1a04a2dbe1
Namit Piriya <34130842+namit-piriya@users.noreply.github.com>
2023-09-22 00:49:26 +0530

    https://leetcode.com/problems/majority-element

--- ArrayProblems.java
+++ ArrayProblems.java
@@ -243,5 +243,25 @@ public class ArrayProblems {
         }
         return indexToStore;
     }
+//    https://leetcode.com/problems/majority-element
+    public int majorityElement(int[] nums) {
+        int candidate = -1;
+        int count = 0;
+
+        for(int n : nums){
+            if(count == 0){
+                candidate = n;
+            }
+
+            if(candidate == n){
+                count++;
+            }
+            else count--;
+
+        }
+
+        return candidate;
+
+    }
 
 }
