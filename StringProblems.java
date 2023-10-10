7889930fbcc59efe807e5ed2ef52b42df42d5de6
Namit Piriya <34130842+namit-piriya@users.noreply.github.com>
2023-10-11 00:27:26 +0530

    https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/

---
+++ StringProblems.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+
+public class StringProblems {
+    public int lengthOfLastWord(String s) {
+//        "   fly me   to   the moon  "
+        int len = s.length() - 1;
+        int counter = len;
+        int ans = 0;
+        while (s.charAt(counter) == ' ' && counter >= 0) {
+            counter--;
+        }
+
+        while (counter >= 0) {
+            if (s.charAt(counter) == ' ') {
+                break;
+            }
+            ans++;
+            counter--;
+        }
+        return ans;
+
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        var first = strs[0];
+        var last = strs[strs.length - 1];
+        int minInd = Math.min(first.length(), last.length());
+
+        StringBuffer s = new StringBuffer();
+
+        for (int i = 0; i < minInd; i++) {
+            if (first.charAt(i) == last.charAt(i)) {
+                s.append(first.charAt(i));
+            } else {
+                break;
+            }
+        }
+        return s.toString();
+    }
+
+//    https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+    public int strStr(String haystack, String needle) {
+
+        if (haystack.length() < needle.length()) {
+            return 0;
+        }
+
+        for (int i = 0; i < haystack.length(); i++) {
+            int startInd;
+            //        "sadbutsad"   "sad"
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                i++;
+                int j = 1;
+                startInd = i;
+                while (i < haystack.length() && j < needle.length()
+                        && haystack.charAt(i++) == needle.charAt(j++)) ;
+                if (j == needle.length()) {
+                    return startInd;
+                }
+            }
+        }
+        return 0;
+
+
+    }
+
+}
