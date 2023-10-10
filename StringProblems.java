f5203d0f05a2f9e5b70e14c72b8b2d4db4b2dd8a
Namit Piriya <34130842+namit-piriya@users.noreply.github.com>
2023-10-11 03:46:31 +0530

    https://leetcode.com/problems/valid-palindrome

--- StringProblems.java
+++ StringProblems.java
@@ -65,4 +65,34 @@ public class StringProblems {
 
     }
 
+//    https://leetcode.com/problems/valid-palindrome
+    public boolean isPalindrome(String s) {
+        if(s.trim().isEmpty()){
+            return true;
+        }
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start <= end){
+
+            if(!Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+            }
+            else {
+                if(Character.toLowerCase(s.charAt(start)) == Character.toLowerCase(s.charAt(end))){
+                    start++;
+                    end--;
+                }
+                else {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
 }
