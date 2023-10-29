11155247c528cd5108d78a0c1db83ecc10441cde
Namit Piriya <34130842+namit-piriya@users.noreply.github.com>
2023-10-29 15:11:13 +0530

    https://leetcode.com/problems/remove-duplicates-from-sorted-array

--- ArrayProblems.java
+++ ArrayProblems.java
@@ -350,5 +350,18 @@ public class ArrayProblems {
         return true;
     }
 
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 1) return 1;
+        int ptr = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[ptr] = nums[i];
+                ptr++;
+            }
+        }
+        return ptr;
+
+    }
+
 
 }
